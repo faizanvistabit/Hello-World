2c632b6c8ca55f7a2fa15a585aaf05a11922c7cf
faizan <>
2019-04-04 10:14:10 +0500

    adding file in folder

---
+++ keltyyy/bc_test.php
@@ -0,0 +1,11 @@
+<?php
+header('Access-Control-Allow-Origin: *');
+header('Access-Control-Allow-Methods: POST, GET, OPTIONS');
+header('Access-Control-Max-Age: 2000'); 
+header('Access-Control-Allow-Headers: Content-Type');
+
+
+
+
+
+?>
